d5908a5f4a2401cf0ea4b31b4a99fb9b85c4a7ae
Krzysztof Mrozinski <2395394M@student.gla.ac.uk>
2020-02-14 11:15:52 +0000

    Chapter 7 pt2

--- rango/forms.py
+++ rango/forms.py
@@ -19,3 +19,14 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+    class PageForm(forms.ModelForm):
+        def clean(self):
+            cleaned_data = self.cleaned_data
+            url = cleaned_data.get('url')
+
+            if url and not url.startswitch('http://'):
+                url = f'http://{url}'
+                cleaned_data['url'] = url
+            
+            return cleaned_data
